ad52026eb10aca512ddad9371e46d86b8a3a7bf6
Student204 <Student204@ETSUSLC105743.etsu.edu>
2022-10-27 13:53:06 -0400

    another update

--- Songapp/models.py
+++ Songapp/models.py
@@ -1,23 +1,34 @@
-from tkinter import CASCADE
+
 from django.db import models
 from datetime import datetime
 
 # Create your models here.
 class Artiste (models.Model):
-    first_name = models.Charfield(max_length = 40)
+    first_name = models.CharField(max_length = 40)
     last_name  = models.CharField(max_length = 40)
     age = models.IntegerField()
+    
+
+    def __str__(self):
+        return(self.first_name)
 
 class Song (models.Model):
     Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     Title = models.CharField(max_length=40)
     date_released = models.DateField(default=datetime.today)   
-    likes = models.CharField()
-    artiste_id=models.IntegerField()
+    likes = models.IntegerField()
+    artisteId=models.IntegerField()
+
+
+    def __str__(self):
+         return(self.Title)
 
 
 class Lyrics (models.Model):
     Song = models.ForeignKey(Song, on_delete=models.CASCADE)
-    content = models.Charfield(max_length=400)
-    song_id = models.IntegerField(max_length=40)
+    content = models.CharField(max_length=400)
+    song_id = models.IntegerField
    
+
+    def __str__(self):
+         return (self.content)
--- Songapp/views.py
+++ Songapp/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
-
+from .models import Artiste, Song, Lyrics
 # Create your views here.
